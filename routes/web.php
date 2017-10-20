8c55d047629b0985c01491459ce346d634483b04
Xin <lovemarks19@gmail.com>
2017-10-20 23:02:03 +0800

    改用 HomeController 回傳

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('home', function () {
-    return view('home.index');
-});
+Route::get('home', 'HomeController@index');
