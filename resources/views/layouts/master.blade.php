bd85305b3e0b8414471bd529d114d70e203d8ec0
Xin <lovemarks19@gmail.com>
2017-10-20 23:27:01 +0800

    建立共用區塊

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,10 +1,11 @@
 <html>
-<head>
-<title>@yield('title') | My Blog</title>
-</head>
-<body>
-<div class="container">
-    @yield('content')
-</div>
-</body>
+    <head>
+        <title>@yield('title') | My Blog</title>
+    </head>
+    <body>
+        @include('layouts.partials.navigation')
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
 </html>
